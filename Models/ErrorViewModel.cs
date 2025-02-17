e19aa6f67b407740f7249646e360c4cdb0100351
Aaryan Gupta <aaryangupta@Aaryans-MacBook-Air.local>
2025-02-16 22:29:19 -0800

    Add ASP.NET MVC web app project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EnterpriseManagementApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
